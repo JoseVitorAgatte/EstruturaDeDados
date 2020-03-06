078e9c3879b2a9b1017d4483d74dc84d7a8433f1
JoseVitorAgatte <jvoaht@gmail.com>
2020-03-06 00:42:47 -0300

    Exercicio Funcoes 11

---
+++ ExeFuncoes11.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void inverter(int vet[], int i){
+	
+	int trocar, n=i-1;
+	
+	for(i=0;i<n;i++){
+		trocar=vet[i];
+    	vet[i]=vet[n];
+    	vet[n]=trocar;
+		n--;
+	}
+	
+}
+
+int main(){
+	int vet[5];
+	int i;
+	
+	for(i=0;i<5;i++){
+		printf("Entre com o %d valor: ", i+1);
+		scanf("%d", &vet[i]);
+		printf("\n");
+	}
+	
+	inverter(vet,i);
+	
+	int n = i;
+	
+	for(i=0;i<n;i++){
+		printf("%d ",vet[i]);
+	}
+	
+	return 0;
+}
