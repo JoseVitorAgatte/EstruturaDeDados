cf041eb041f115da3fc954e23a48e55a60f5f553
JoseVitorAgatte <jvoaht@gmail.com>
2020-03-06 00:45:31 -0300

    Exercicio Funcoes 14

---
+++ ExeFuncoes14.c
@@ -0,0 +1,33 @@
+void decrescente(int vet[],int n) {
+	
+	int troca, i, j;
+	
+    for(i=0;i<n;i++){
+    	for(j=i+1;j<n;j++){
+    		if(vet[i]<vet[j]){
+    			troca=vet[i];
+				vet[i]=vet[j];
+				vet[j]=troca;	
+			} 
+		}
+	}	
+ 
+ }
+
+ int main() {
+ 	int vet[5];
+ 	int i;
+ 	
+	for(i=0;i<5;i++){
+		printf("Entre com o %d valor: ", i+1);
+		scanf("%d", &vet[i]);
+		printf("\n");
+	}
+	decrescente(vet,5);
+	 
+	for(i=0;i<5;i++){
+ 		printf("%d ", vet[i]);
+	}
+	 
+	return 0;
+}
